6eceaa55e60c855dd1464c26bf24dda145317abe
SerhiiCho <serhiicho@protonmail.com>
2024-01-31 21:03:48 +0200

    Change DatabaseSeeder.php by adding more posts and tags

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
     {
         $user = User::factory()->create();
 
-        $posts = Post::factory(100)->create(['user_id' => $user]);
+        $posts = Post::factory(1000)->create(['user_id' => $user]);
 
         $tags = collect([
             Tag::factory()->create(['name' => 'Laravel'])->id,
@@ -21,10 +21,29 @@ class DatabaseSeeder extends Seeder
             Tag::factory()->create(['name' => 'JavaScript'])->id,
             Tag::factory()->create(['name' => 'Vue.js'])->id,
             Tag::factory()->create(['name' => 'React.js'])->id,
+            Tag::factory()->create(['name' => 'Tailwind CSS'])->id,
+            Tag::factory()->create(['name' => 'Alpine.js'])->id,
+            Tag::factory()->create(['name' => 'Livewire'])->id,
+            Tag::factory()->create(['name' => 'Inertia.js'])->id,
+            Tag::factory()->create(['name' => 'Svelte'])->id,
+            Tag::factory()->create(['name' => 'Ruby on Rails'])->id,
+            Tag::factory()->create(['name' => 'Python'])->id,
+            Tag::factory()->create(['name' => 'Django'])->id,
+            Tag::factory()->create(['name' => 'Flask'])->id,
+            Tag::factory()->create(['name' => 'SASS'])->id,
+            Tag::factory()->create(['name' => 'CSS'])->id,
+            Tag::factory()->create(['name' => 'HTML'])->id,
+            Tag::factory()->create(['name' => 'Tailwind UI'])->id,
+            Tag::factory()->create(['name' => 'Alpine UI'])->id,
+            Tag::factory()->create(['name' => 'Laravel UI'])->id,
+            Tag::factory()->create(['name' => 'Jetstream'])->id,
+            Tag::factory()->create(['name' => 'Fortify'])->id,
+            Tag::factory()->create(['name' => 'Sail'])->id,
+            Tag::factory()->create(['name' => 'Nova'])->id,
         ]);
 
         $posts->each(function (Post $post) use ($tags) {
-            $post->tags()->attach($tags->random(2));
+            $post->tags()->attach($tags->random(random_int(1, 4)));
         });
     }
 }
