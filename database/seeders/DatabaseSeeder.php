4bc1fa368979bc8669561fe4b68aec6fa959120a
SerhiiCho <serhiicho@protonmail.com>
2024-01-31 20:41:30 +0200

    Add relationship for Post Tag models
    Add seeding 100 posts with tags

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,21 +2,29 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Post;
+use App\Models\Tag;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        $user = User::factory()->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $posts = Post::factory(100)->create(['user_id' => $user]);
+
+        $tags = collect([
+            Tag::factory()->create(['name' => 'Laravel'])->id,
+            Tag::factory()->create(['name' => 'PHP'])->id,
+            Tag::factory()->create(['name' => 'JavaScript'])->id,
+            Tag::factory()->create(['name' => 'Vue.js'])->id,
+            Tag::factory()->create(['name' => 'React.js'])->id,
+        ]);
+
+        $posts->each(function (Post $post) use ($tags) {
+            $post->tags()->attach($tags->random(2));
+        });
     }
 }
